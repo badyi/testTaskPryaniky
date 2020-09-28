e206e8371767dc8d56f6abcfcf8899a6cdf9d25f
badyi <badyi7@icloud.com>
2020-09-28 23:05:50 +0300

    Initial Commit

---
+++ testTaskPryaniky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTaskPryaniky
+//
+//  Created by и on 28.09.2020.
+//  Copyright © 2020 badyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
